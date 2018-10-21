a286a87ff102f3b35c6e651c0c3a22db692d68c9
RealWeeks <weeksjasons@gmail.com>
2018-10-20 20:55:28 -0400

    change controller to fat arrow

--- controllers/events.js
+++ controllers/events.js
@@ -2,74 +2,74 @@
 const mongoose = require('mongoose')
 const Event = mongoose.model('Events')
 
-const Moment = require('moment');
-const MomentRange = require('moment-range');
+const Moment = require('moment')
+const MomentRange = require('moment-range')
 
-const moment = MomentRange.extendMoment(Moment);
+const moment = MomentRange.extendMoment(Moment)
 
-exports.get_events = function(req, res) {
-  Event.find({}, function(err, event) {
-    if (err)
-      res.send(err);
-    res.json(event);
-  });
-};
+exports.get_events = (req, res) => {
+  Event.find({}, (err, event) => {
+    if (err)res.send(err);
+    res.json(event)
+  })
+}
 
-exports.create_event = function(req, res) {
+exports.create_event = (req, res) => {
   if (req.body.event_type !== 'away') {
-    Event.findOne({person : req.body.person, date_range: req.body.start}, function(err, data){
+    Event.findOne({person : req.body.person, date_range: req.body.start}, (err, data) =>{
       if(err){res.send(err)}
 
       if(!data) {
         let new_event = new Event(req.body)
-        new_event.save(function(err, event) {
+        new_event.save((err, event) => {
           if (err){
             res.send(err)
           }
           res.json(event)
         })
       }else{
-        res.send({'error': 'Not created. Person is away'});
+        res.send({'error': 'Not created. Person is away'})
       }
     })
 
   }else{
-    let new_event = new Event(req.body);
-    new_event.save(function(err, event) {
+    let new_event = new Event(req.body)
+    new_event.save((err, event) => {
       if (err){
         res.send(err)
       }
       res.json(event)
-    });
+    })
   }
 
-};
+}
 
 
-exports.read_event = function(req, res) {
-  Event.findById(req.params.event_id, function(err, event) {
+exports.read_event = (req, res) => {
+  Event.findById(req.params.event_id, (err, event) => {
     if (err)
-      res.send(err);
-    res.json(event);
-  });
-};
+      res.send(err)
+    res.json(event)
+  })
+}
 
 
-exports.update_event = function(req, res) {
-  Event.findOneAndUpdate({_id: req.params.event_id}, req.body, {new: true}, function(err, event) {
+exports.update_event = (req, res) => {
+  Event.findOneAndUpdate({_id: req.params.event_id}, req.body, {new: true}, (err, event) => {
     if (err)
-      res.send(err);
-    res.json(event);
-  });
-};
+      res.send(err)
+    res.json(event)
+  })
+}
 
 
-exports.delete_event = function(req, res) {
+exports.delete_event = (req, res) => {
   Event.remove({
     _id: req.params.event_id
-  }, function(err, event) {
+  }, (err, event) => {
     if (err)
-      res.send(err);
-    res.json({ message: 'Deleted' });
-  });
-};
+      res.send(err)
+
+    res.json({ message: 'Deleted' })
+  })
+}
